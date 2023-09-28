1e71e238f8c736a15f110a5414e6c9a334671c0a
Moe Green <gearmobile@gmail.com>
2023-09-28 20:59:16 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sng-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'signals';
-}
+export class AppComponent {}
